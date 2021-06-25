5d230fedbb7dd59a0674f14937aa93d2414cab69
AdItYa302007 <66154816+AdItYa302007@users.noreply.github.com>
2021-06-25 20:44:35 +0530

    Add files via upload

---
+++ FilesRemover.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+import time
+
+def delete():
+    deleted_folders_count = 0
+    deleted_files_count = 0
+	
+
+    path = "/PATH_TO_DELETE"
+    days = 30
+    seconds = time.time() - (days * 24 * 60 * 60)
+
+  
+
